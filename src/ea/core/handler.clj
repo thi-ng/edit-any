3aab82178ab0d9475748c7b1532519b46b7ed3f6
Karsten Schmidt <k@postspectacular.com>
2014-11-15 21:05:33 +0000

    update modified date handling (store as formatted date), reformat table gen code

--- src/ea/core/handler.clj
+++ src/ea/core/handler.clj
@@ -8,6 +8,11 @@
    [thi.ng.trio.query :as q]
    [clojure.pprint :refer [pprint]]))
 
+(def date-format (java.text.SimpleDateFormat. "yyyy-MM-dd'T'HH:mm:ss.SSSZ"))
+
+(defn format-timestamp
+  [t] (.format date-format (java.util.Date. t)))
+
 (defonce graph (ref (trio/plain-store)))
 
 (defn resource-link
@@ -29,49 +34,54 @@
                               :from @graph
                               :query [{:where [[id "dcterms:content" '?body]]}
                                       {:optional [[id "rdfs:label" '?title]]}]}))
-                     page (q/query
-                           {:select :*
-                            :from @graph
-                            :query [{:where [[id '?att '?val]]
-                                     :filter {'?att #(not (#{"dcterms:content" "rdfs:label"} %))}}
-                                    {:optional [['?att "rdfs:label" '?atitle]]}
-                                    {:optional [['?val "rdfs:label" '?vtitle]]}]
-                            :order-asc '[?att ?val]})
-                     others (q/query
-                             {:select :*
-                              :from @graph
-                              :query [{:where [['?other id '?val]]}
-                                      {:optional [['?other "rdfs:label" '?otitle]]}
-                                      {:optional [['?val "rdfs:label" '?vtitle]]}]
-                              :order-asc '[?other ?val]})
-                     others2 (q/query
+                     attribs (q/query
                               {:select :*
                                :from @graph
-                               :query [{:where [['?other '?pred id]]}
-                                       {:optional [['?other "rdfs:label" '?otitle]]}
-                                       {:optional [['?pred "rdfs:label" '?ptitle]]}]
-                               :order-asc '[?other ?pred]})]
+                               :query [{:where [[id '?att '?val]]
+                                        :filter {'?att #(not (#{"dcterms:content" "rdfs:label"} %))}}
+                                       {:optional [['?att "rdfs:label" '?atitle]]}
+                                       {:optional [['?val "rdfs:label" '?vtitle]]}]
+                               :order-asc '[?att ?val]})
+                     shared-pred (q/query
+                                  {:select :*
+                                   :from @graph
+                                   :query [{:where [['?other id '?val]]}
+                                           {:optional [['?other "rdfs:label" '?otitle]]}
+                                           {:optional [['?val "rdfs:label" '?vtitle]]}]
+                                   :order-asc '[?other ?val]})
+                     shared-obj (q/query
+                                 {:select :*
+                                  :from @graph
+                                  :query [{:where [['?other '?pred id]]}
+                                          {:optional [['?other "rdfs:label" '?otitle]]}
+                                          {:optional [['?pred "rdfs:label" '?ptitle]]}]
+                                  :order-asc '[?other ?pred]})]
                  (str
                   "<h1>" (or ?title id) "</h1>"
                   "<textarea>" ?body "</textarea>"
-                  (when (seq page)
+                  (when (seq attribs)
                     (str "<h2>Attribs</h2>"
                          "<table>"
-                         (->> page
+                         (->> attribs
                               (map (fn [{:syms [?att ?atitle ?val ?vtitle]}]
-                                     (str "<tr><td>" (resource-link ?att ?atitle) "</td><td>" (resource-link ?val ?vtitle) "</td></tr>")))
+                                     (str "<tr><td>" (resource-link ?att ?atitle) "</td>"
+                                          "<td>" (resource-link ?val ?vtitle) "</td></tr>")))
                               (apply str))
                          "</table>"))
-                  (when (or (seq others) (seq others2))
+                  (when (or (seq shared-pred) (seq shared-obj))
                     (str "<h2>Other resources</h2>"
                          "<table>"
-                         (->> others
+                         (->> shared-pred
                               (map (fn [{:syms [?other ?otitle ?val ?vtitle]}]
-                                     (str "<tr><td>" (resource-link ?other ?otitle) "</td><td>" (or ?title id) "</td><td>" (resource-link ?val ?vtitle) "</td></tr>")))
+                                     (str "<tr><td>" (resource-link ?other ?otitle) "</td>"
+                                          "<td>" (or ?title id) "</td>"
+                                          "<td>" (resource-link ?val ?vtitle) "</td></tr>")))
                               (apply str))
-                         (->> others2
+                         (->> shared-obj
                               (map (fn [{:syms [?other ?otitle ?pred ?ptitle]}]
-                                     (str "<tr><td>" (resource-link ?other ?otitle) "</td><td>" (resource-link ?pred ?ptitle) "</td><td>" (or ?title id) "</td></tr>")))
+                                     (str "<tr><td>" (resource-link ?other ?otitle) "</td>"
+                                          "<td>" (resource-link ?pred ?ptitle) "</td>"
+                                          "<td>" (or ?title id) "</td></tr>")))
                               (apply str))
                          "</table>")))))
   :post! (fn [ctx]
@@ -80,7 +90,7 @@
                   triples (->> (map (fn [[p o]] [id (name p) o]) attribs)
                                (concat [[id "dcterms:content" body]
                                         [id "rdfs:label" title]
-                                        [id "dcterms:modified" (System/currentTimeMillis)]]))]
+                                        [id "dcterms:modified" (format-timestamp (System/currentTimeMillis))]]))]
               (prn :triples triples)
               (alter graph trio/add-triples triples)
               (spit "graph.edn" (sort (trio/select @graph))))))
