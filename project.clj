725caf4ca2d6c159ad83da4a2a7826db730bc051
Karsten Schmidt <k@postspectacular.com>
2014-11-15 20:47:56 +0000

    Initial import

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject ea "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.2.0"]
+                 [ring/ring-defaults "0.1.2"]
+                 [liberator "0.12.2"]
+                 [thi.ng/trio "0.1.0-SNAPSHOT"]]
+  :plugins [[lein-ring "0.8.13"]]
+  :ring {:handler ea.core.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
---
+++ src/ea/core/handler.clj
@@ -0,0 +1,93 @@
+(ns ea.core.handler
+  (:require
+   [compojure.core :refer :all]
+   [compojure.route :as route]
+   [liberator.core :as lib :refer [defresource]]
+   [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+   [thi.ng.trio.core :as trio]
+   [thi.ng.trio.query :as q]
+   [clojure.pprint :refer [pprint]]))
+
+(defonce graph (ref (trio/plain-store)))
+
+(defn resource-link
+  [id & [label]]
+  (let [id (if (keyword? id) (name id) id)]
+    (str "<a href=\"/resources/" id "\">" (or label id) "</a>")))
+
+(defn maybe-number
+  [x] (try (Long/parseLong x) (catch Exception e x)))
+
+(defresource page [id]
+  :available-media-types ["text/html" "application/edn"]
+  :allowed-methods [:get :post :put]
+  :handle-ok (fn [ctx]
+               (let [id (maybe-number id)
+                     {:syms [?body ?title] :as res}
+                     (first (q/query
+                             {:select :*
+                              :from @graph
+                              :query [{:where [[id "dcterms:content" '?body]]}
+                                      {:optional [[id "rdfs:label" '?title]]}]}))
+                     page (q/query
+                           {:select :*
+                            :from @graph
+                            :query [{:where [[id '?att '?val]]
+                                     :filter {'?att #(not (#{"dcterms:content" "rdfs:label"} %))}}
+                                    {:optional [['?att "rdfs:label" '?atitle]]}
+                                    {:optional [['?val "rdfs:label" '?vtitle]]}]
+                            :order-asc '[?att ?val]})
+                     others (q/query
+                             {:select :*
+                              :from @graph
+                              :query [{:where [['?other id '?val]]}
+                                      {:optional [['?other "rdfs:label" '?otitle]]}
+                                      {:optional [['?val "rdfs:label" '?vtitle]]}]
+                              :order-asc '[?other ?val]})
+                     others2 (q/query
+                              {:select :*
+                               :from @graph
+                               :query [{:where [['?other '?pred id]]}
+                                       {:optional [['?other "rdfs:label" '?otitle]]}
+                                       {:optional [['?pred "rdfs:label" '?ptitle]]}]
+                               :order-asc '[?other ?pred]})]
+                 (str
+                  "<h1>" (or ?title id) "</h1>"
+                  "<textarea>" ?body "</textarea>"
+                  (when (seq page)
+                    (str "<h2>Attribs</h2>"
+                         "<table>"
+                         (->> page
+                              (map (fn [{:syms [?att ?atitle ?val ?vtitle]}]
+                                     (str "<tr><td>" (resource-link ?att ?atitle) "</td><td>" (resource-link ?val ?vtitle) "</td></tr>")))
+                              (apply str))
+                         "</table>"))
+                  (when (or (seq others) (seq others2))
+                    (str "<h2>Other resources</h2>"
+                         "<table>"
+                         (->> others
+                              (map (fn [{:syms [?other ?otitle ?val ?vtitle]}]
+                                     (str "<tr><td>" (resource-link ?other ?otitle) "</td><td>" (or ?title id) "</td><td>" (resource-link ?val ?vtitle) "</td></tr>")))
+                              (apply str))
+                         (->> others2
+                              (map (fn [{:syms [?other ?otitle ?pred ?ptitle]}]
+                                     (str "<tr><td>" (resource-link ?other ?otitle) "</td><td>" (resource-link ?pred ?ptitle) "</td><td>" (or ?title id) "</td></tr>")))
+                              (apply str))
+                         "</table>")))))
+  :post! (fn [ctx]
+           (dosync
+            (let [{:keys [body title attribs]} (get-in ctx [:request :params])
+                  triples (->> (map (fn [[p o]] [id (name p) o]) attribs)
+                               (concat [[id "dcterms:content" body]
+                                        [id "rdfs:label" title]
+                                        [id "dcterms:modified" (System/currentTimeMillis)]]))]
+              (prn :triples triples)
+              (alter graph trio/add-triples triples)
+              (spit "graph.edn" (sort (trio/select @graph))))))
+  :post-redirect? (fn [ctx] {:location (str "/resources/" id)}))
+
+(defroutes app-routes
+  (ANY "/resources/:id" [id] (page id)))
+
+(def app
+  (wrap-defaults app-routes (assoc-in site-defaults [:security :anti-forgery] false)))
